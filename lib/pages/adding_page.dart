a863c96b4b0aadeaf7e3cbeeef0f881d977afb1d
Mike Linker <michaellinker@hotmail.com>
2025-02-05 22:42:19 -0300

    New page for adding new To Dos

--- lib/pages/adding_page.dart
+++ lib/pages/adding_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/models/to_do.dart';
 
 class AddingPage extends StatelessWidget {
-  const AddingPage({super.key});
-
+  AddingPage({super.key});
+  final toDoTitleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,9 +13,12 @@ class AddingPage extends StatelessWidget {
       body: Card(
         child: Column(children: [
           Text('ToDo Title:'),
-          TextField(),
+          TextField(controller: toDoTitleController),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              final new_todo = ToDo(title: toDoTitleController.text, done: false);
+              Navigator.of(context).pop(new_todo);
+            },
             child: Text('Add'),
           ),
         ]),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/models/to_do.dart';
 import 'package:to_do_list/widgets/todo_tile.dart';
+import 'package:to_do_list/pages/adding_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> { 
   List<ToDo> toDos = [
     ToDo(title: 'Buy groceries'),
     ToDo(title: 'Walk the dog', done: true),
@@ -20,6 +21,18 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('To Do List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () async {
+              final newToDo = (await Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  AddingPage())))as ToDo; //learn about casting
+              setState(() {
+                toDos.add(newToDo);
+              });
+            },
+          ),
+          TextButton(onPressed: (){}, child: Text('Add')),
+        ],
       ),
       body: ListView.builder(
         itemCount: toDos.length,
